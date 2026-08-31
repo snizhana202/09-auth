3d0c9737ae4b797c9831d7171f5442096f6e8b46
Snizhana <snizhanapetrushka@gmail.com>
2026-08-31 10:08:28 +0200

    style (footer): update footer styling

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,15 +3,13 @@ import css from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={css.footer}>
-      <div className={css.content}>
-        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
-        <div className={css.wrap}>
-          <p>Developer: Snizhana</p>
-          <p>
-            Contact us:
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
-          </p>
-        </div>
+      <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+      <div className={css.wrap}>
+        <p>Developer: Snizhana</p>
+        <p>
+          Contact us:
+          <a href="mailto:student@notehub.app">student@notehub.app</a>
+        </p>
       </div>
     </footer>
   );
