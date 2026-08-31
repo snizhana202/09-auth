f7b18a6732d5ab2016cfc764eec9c187d330097f
Snizhana <snizhanapetrushka@gmail.com>
2026-08-31 10:27:59 +0200

    style (sidebar): restyle sidebar, fix full-height background, add active link state

--- app/(private routes)/notes/filter/@sidebar/default.tsx
+++ app/(private routes)/notes/filter/@sidebar/default.tsx
@@ -1,21 +1,33 @@
-// app/notes/filter/@sidebar/default.tsx
+"use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import css from "@/components/SidebarNotes/SidebarNotes.module.css";
 
 export default function NotesSidebar() {
+  const pathname = usePathname();
   const tags = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
+
+  const isActive = (tag: string) => pathname === `/notes/filter/${tag}`;
 
   return (
     <ul className={css.menuList}>
       <li className={css.menuItem}>
-        <Link href={`/notes/filter/all`} className={css.menuLink}>
+        <Link
+          href={`/notes/filter/all`}
+          className={`${css.menuLink} ${
+            pathname === "/notes/filter/all" ? css.active : ""
+          }`}
+        >
           All notes
         </Link>
       </li>
       {tags.map((tag) => (
         <li key={tag} className={css.menuItem}>
-          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+          <Link
+            href={`/notes/filter/${tag}`}
+            className={`${css.menuLink} ${isActive(tag) ? css.active : ""}`}
+          >
             {tag}
           </Link>
         </li>
